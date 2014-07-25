ba1336d49986a8f05931d0051cd5d8542d0f9192
Wassif <wassifaziz@gmail.com>
2014-07-25 11:45:39 +0100

    included files
    Added project files, this is BB's latest build

---
+++ js/builder.js
@@ -0,0 +1,89 @@
+$(window).load(function() {
+
+    var width = 700;
+    var column_width = 20;
+    var main_column_width = 700;
+
+
+    //        var width=prompt("Set email width");
+    $('.main_container_table').attr('width', width);
+    //
+    //         var column_width=prompt("Set column width");
+    $('.margin_column').attr('width', column_width);
+    //
+    //        var total_column_width = column_width*2;
+    //        var main_column_width = width - total_column_width;
+    //        //alert(new_container_width);
+    $('.main_column').attr('width', main_column_width);
+
+    
+    
+    $(function() {
+        var tbdiv = $('.builder-table'); //was #draggable table
+        $(tbdiv).draggable({
+            connectToSortable: ".sortable",
+            helper: "clone",
+            revert: "invalid"
+        });
+
+        $(".sortable").sortable({
+            revert: true,
+            receive: function(event, ui) {
+                var html = [];
+            }
+        });
+
+    });
+
+
+    setInterval(function() {
+
+        $('.main_row').each(function() {
+
+            var TabIn = $('.sortable').find('table').length;
+
+            if (TabIn >= 1) {
+                console.log('has ' + TabIn);
+
+                //$(".sortable").sortable('refresh');
+
+                $(this).removeClass('main_row');
+
+                $(this).find('td').removeClass('sortable');
+
+                $(this).find('td').removeClass('ui-sortable');
+
+                $(this).after('<tr class="main_row"><td bgcolor="#FFFFFF" class="sortable" id=""></td></tr>');
+
+                $(".sortable").sortable({
+                    revert: true,
+                    receive: function(event, ui) {
+                        var html = [];
+                    }
+                });
+
+            } else {
+                console.log('no table');
+            }
+
+        });
+
+    }, 1000);
+
+
+    //INLINE EDIT SCRIPT
+    $(function() {
+        $('.editable').inlineEdit({
+            control: 'textarea'
+        });
+    });
+
+
+    /*$('.loader').on('click', function(){
+        $('.generated').load('week.html');
+    });*/
+    
+    
+    
+
+});//END LOAD FUNCTION
